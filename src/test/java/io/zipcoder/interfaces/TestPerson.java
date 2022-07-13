cdd174220a57522db31efa48e928ef091f0b28b0
eugeneshine1 <eugeneshine1@gmail.com>
2022-07-13 14:14:11 -0400

    part 1

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,34 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+    public void testConstructor() {
+        //given
+        long expectedId = 12;
+        String expectedName ="Eugene";
+        //when
+        Person testPerson = new Person(expectedId,expectedName);
+        // then
+        Assert.assertEquals(expectedId,testPerson.getId());
+        Assert.assertEquals(expectedName,testPerson.getName());
+    }
+
+    @Test
+    public void testSetName() {
+        //given
+        long expectedId = 12;
+        String expectedName ="Eugene";
+        //when
+        Person testPerson = new Person(expectedId,expectedName);
+        String newName = "Paul";
+        testPerson.setName(newName);
+        String actual = testPerson.getName();
+        // then
+        Assert.assertEquals(newName,actual);
+    }
 
 }
